9c4904325be067246e23b5ec0d115d887713999f
Severin Bruhat <sbruhat@gmail.com>
2022-11-22 21:15:18 +0000

    add site srtucture

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
 
+// start the server
 const server = app.listen(8000, () => {
     console.log(`Express running → PORT ${server.address().port}`);
   });
 
-  //load template engine
-  app.set('view engine', 'pug');
+//load template engine
+app.set('view engine', 'pug');
+
+// serve static files from the `public` folder
+app.use(express.static(__dirname + '/public'));
+
 
   app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {
+        title: 'Homepage'
+    })
   });
 
