d8ad407b42897b066c7b9d2d942bdb4dd26dce15
Severin Bruhat <sbruhat@gmail.com>
2022-11-22 21:11:34 +0000

    add the template engine (Pug)

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
 
-const server = app.listen(7000, () => {
+const server = app.listen(8000, () => {
     console.log(`Express running → PORT ${server.address().port}`);
+  });
+
+  //load template engine
+  app.set('view engine', 'pug');
+
+  app.get('/', (req, res) => {
+    res.render('index')
   });
 
